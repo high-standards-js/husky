8ce5899ae2dded26c6bf8db2112d5e333e62557b
Oliver Lorenz <mail@oliverlorenz.com>
2020-06-18 18:21:54 +0200

    feat: initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const base = require('@high-standards-js/base');
+
+(async() => {
+    await base.checkAcceptedHighStandards();
+
+    let packageJsonOfConfig = base.getInitiatingProjectPackageJson();
+    
+    if (!packageJsonOfConfig.engines) packageJsonOfConfig.engines = {};
+    if (!packageJsonOfConfig.engines.node) packageJsonOfConfig.engines.node = `>=${process.versions.node}`;
+    
+    base.writeFile(
+        'commitlint.config.js', 
+        base.getTemplate(
+            __dirname, 
+            'commitlint.config.js'
+        )
+    )
+
+    packageJsonOfConfig = base.addDependency(packageJsonOfConfig, 'husky');
+
+    if (!packageJsonOfConfig.husky) packageJsonOfConfig.husky = {};
+    if (!packageJsonOfConfig.husky.hooks) packageJsonOfConfig.husky.hooks = {};
+    
+    base.writeInitiatingProjectPackageJson(packageJsonOfConfig);
+})()
