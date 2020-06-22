a0bb891aa7ae166175d618c635c6d8707a9893ba
Oliver Lorenz <mail@oliverlorenz.com>
2020-06-22 12:21:52 +0200

    fix: bump versions

--- src/postinstall.js
+++ src/postinstall.js
@@ -1,25 +1,14 @@
 const base = require('@high-standards-js/base');
 
-(async() => {
-    await base.checkAcceptedHighStandards();
+(async () => {
+  await base.checkAcceptedHighStandards();
 
-    let packageJsonOfConfig = base.getInitiatingProjectPackageJson();
-    
-    if (!packageJsonOfConfig.engines) packageJsonOfConfig.engines = {};
-    if (!packageJsonOfConfig.engines.node) packageJsonOfConfig.engines.node = `>=${process.versions.node}`;
-    
-    base.writeFile(
-        'commitlint.config.js', 
-        base.getTemplate(
-            __dirname, 
-            'commitlint.config.js'
-        )
-    );
+  let packageJson = base.getInitiatingProjectPackageJson();
 
-    packageJsonOfConfig = await base.addDevDependency(packageJsonOfConfig, 'husky');
+  packageJson = await base.addDevDependency(packageJson, 'husky');
 
-    if (!packageJsonOfConfig.husky) packageJsonOfConfig.husky = {};
-    if (!packageJsonOfConfig.husky.hooks) packageJsonOfConfig.husky.hooks = {};
+  if (!packageJson.husky) packageJson.husky = {};
+  if (!packageJson.husky.hooks) packageJson.husky.hooks = {};
 
-    base.writeInitiatingProjectPackageJson(packageJsonOfConfig);
-})()
+  base.writeInitiatingProjectPackageJson(packageJson);
+})();
